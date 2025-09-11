0fa6998a52ac72fb40dd422970f9ac27c6015438
kksat <22549266+kksat@users.noreply.github.com>
2025-09-11 13:28:05 +0200

    use subnet ids, not CIDRs
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

--- rosa/terraform/rosa.tf
+++ rosa/terraform/rosa.tf
@@ -4,6 +4,14 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+data "aws_subnets" "subnets" {
+  region = var.aws_region
+  filter {
+    name   = "tag:Name"
+    values = [var.vpc_name]
+  }
+}
+
 module "rosa-hcp" {
   source                 = "git::https://github.com/terraform-redhat/terraform-rhcs-rosa-hcp.git?ref=68c20d8"
   cluster_name           = var.cluster_name
@@ -14,7 +22,7 @@ module "rosa-hcp" {
   aws_availability_zones = slice([for zone in data.aws_availability_zones.available.names : format("%s", zone)], 0, 3)
   create_oidc            = true
   private                = true
-  aws_subnet_ids         = concat(var.private_subnets, var.public_subnets)
+  aws_subnet_ids         = data.aws_subnets.subnets.ids
   create_account_roles   = true
   create_operator_roles  = true
 }
